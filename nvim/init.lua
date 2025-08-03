ea2643748bf3744dcb98db2ef617ddfd13ac8d70
Prashant Koirala <92243577+prashantkoirala465@users.noreply.github.com>
2025-08-04 00:03:34 +0545

    feat: updated config files

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,7 @@
-require("josean.core")
-require("josean.lazy")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+-- Setup Neovide
+if vim.g.neovide then
+  require("config.neovide")
+end
